79a46dfccdb222c992a287f4e6039b918ee9dd0c
ThaSMorato <tha.s.morato@gmail.com>
2020-09-30 23:56:42 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config);
+
+/* Call API.graphql, passing in the query that we'd like to execute. */
+//const postData = await API.graphql({ query: listPosts });
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
